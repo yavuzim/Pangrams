ef6a326f09dba1d13e2b895814ae76696f4b67f6
kemal <yavuz.imer133@gmail.com>
2021-05-23 20:17:37 +0300

    Pangrams

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string Pangrams(string sentence)
+        {
+            List<string> letters = new List<string>();
+
+            for (int i = 0; i < sentence.Length; i++)
+                if (char.IsLetter(sentence[i]))
+                    if (!letters.Contains(sentence[i].ToString()))
+                        letters.Add(sentence[i].ToString().ToLower());
+            return letters.Count == 29 ? "Pangrams" : "Not Prangrams";
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Sentence : ");
+            string sentence = Console.ReadLine();
+
+            string getvalue = Pangrams(sentence);
+            Console.Write("Screen Output : {0}", getvalue);
+            Console.Read();
+        }
+    }
+}
